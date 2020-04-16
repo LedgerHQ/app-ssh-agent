c2eb189a902155887ce6620d3ff287082aa8ef9a
greenknot <greenknot@users.noreply.github.com>
2020-04-16 11:25:20 +0200

    tests: use apdu_exchange() from ledgerwallet

--- tests.py
+++ tests.py
@@ -6,7 +6,7 @@ import sys
 import time
 from enum import IntEnum
 
-from ledgerwallet.client import LedgerClient
+from ledgerwallet.client import LedgerClient, CommException
 from ledgerwallet.crypto.ecc import PrivateKey
 from ledgerwallet.params import Bip32Path
 from ledgerwallet.transport import enumerate_devices
@@ -33,15 +33,6 @@ class Curve(IntEnum):
     INVALID_03        = 0x03
     PUBLIC_KEY_MARKER = 0x80
 
-def build_apdu(ins, payload, p1, curve):
-    data = CLA.to_bytes(1, "big")
-    data += ins.to_bytes(1, "big")
-    data += p1.to_bytes(1, "big")
-    data += curve.to_bytes(1, "big")
-    data += len(payload).to_bytes(1, "big")
-    data += payload
-    return data
-
 @pytest.fixture(scope="module")
 def client():
     devices = enumerate_devices()
@@ -49,78 +40,66 @@ def client():
         print("No Ledger device has been found.")
         sys.exit(0)
 
-    return LedgerClient(devices[0])
+    return LedgerClient(devices[0], cla=CLA)
 
-class Client:
-    def apdu_exchange(self, client, payload=b"", p1=P1.FIRST, curve=Curve.PRIME256):
-        apdu = build_apdu(self.INS, payload, p1, curve)
-        response = client.raw_exchange(apdu)
-        # some hangs were noticed with speculos otherwise
-        time.sleep(0.2)
-        status_word = int.from_bytes(response[-2:], "big")
-        return status_word, response[:-2]
-
-class TestGetPublicKey(Client):
+class TestGetPublicKey:
     INS = Ins.GET_PUBLIC_KEY
 
     def test_get_public_key(self, client):
         path = Bip32Path.build(DEFAULT_PATH)
-        status_word, _ = self.apdu_exchange(client, path)
-        assert status_word == 0x9000
+        client.apdu_exchange(self.INS, path, P1.FIRST, Curve.PRIME256)
 
     def test_path_too_long(self, client):
         path = Bip32Path.build(DEFAULT_PATH + "/0/0/0/0/0/0")
-        status_word, _ = self.apdu_exchange(client, path)
-        assert status_word == 0x6a80
+        with pytest.raises(CommException) as e:
+            client.apdu_exchange(self.INS, path, P1.FIRST, Curve.PRIME256)
+        assert e.value.sw == 0x6a80
 
-class TestSignGenericHash(Client):
+class TestSignGenericHash:
     INS = Ins.SIGN_GENERIC_HASH
 
     def test_sign_generic_hash(self, client):
         payload = Bip32Path.build(DEFAULT_PATH) + b"a"
-        status_word, _ = self.apdu_exchange(client, payload)
-        assert status_word == 0x9000
-
-        status_word, _ = self.apdu_exchange(client, b"b", p1=P1.NEXT)
-        assert status_word == 0x9000
-
-        status_word, _ = self.apdu_exchange(client, b"b", p1=P1.LAST)
-        assert status_word == 0x9000
+        client.apdu_exchange(self.INS, payload, P1.FIRST, Curve.PRIME256)
+        client.apdu_exchange(self.INS, b"b", P1.NEXT, Curve.PRIME256)
+        client.apdu_exchange(self.INS, b"b", P1.LAST, Curve.PRIME256)
 
     def test_invalid_curve(self, client):
-        status_word, _ = self.apdu_exchange(client, curve=Curve.INVALID_03)
-        assert status_word == 0x6b00
+        with pytest.raises(CommException) as e:
+            client.apdu_exchange(self.INS, sw1=P1.FIRST, sw2=Curve.INVALID_03)
+        assert e.value.sw == 0x6b00
 
-class TestSignDirectHash(Client):
+class TestSignDirectHash:
     INS = Ins.SIGN_DIRECT_HASH
 
     def test_sign_direct_hash(self, client):
         payload = Bip32Path.build(DEFAULT_PATH) + b"a" * 32
-        status_word, _ = self.apdu_exchange(client, payload)
-        assert status_word == 0x9000
+        client.apdu_exchange(self.INS, payload, P1.FIRST, Curve.PRIME256)
 
     def test_invalid_hash_len(self, client):
         payload = Bip32Path.build(DEFAULT_PATH) + b"a"
-        status_word, _ = self.apdu_exchange(client, payload)
-        assert status_word == 0x6700
+        with pytest.raises(CommException) as e:
+            client.apdu_exchange(self.INS, payload, sw1=P1.FIRST, sw2=Curve.PRIME256)
+        assert e.value.sw == 0x6700
 
     def test_invalid_steps(self, client):
-        status_word, _ = self.apdu_exchange(client, p1=P1.NEXT)
-        assert status_word == 0x6b00
+        with pytest.raises(CommException) as e:
+            client.apdu_exchange(self.INS, sw1=P1.NEXT, sw2=Curve.PRIME256)
+        assert e.value.sw == 0x6b00
 
-        status_word, _ = self.apdu_exchange(client, p1=P1.LAST)
-        assert status_word == 0x6b00
+        with pytest.raises(CommException) as e:
+            client.apdu_exchange(self.INS, sw1=P1.LAST, sw2=Curve.PRIME256)
+        assert e.value.sw == 0x6b00
 
-class TestGetECDHSecret(Client):
+class TestGetECDHSecret:
     INS = Ins.GET_ECDH_SECRET
 
     def test_get_ecdh_secret(self, client):
         pubkey = PrivateKey().pubkey.serialize(compressed=False)
         payload = Bip32Path.build(DEFAULT_PATH) + pubkey
-        status_word, _ = self.apdu_exchange(client, payload)
-        assert status_word == 0x9000
+        client.apdu_exchange(self.INS, payload, sw1=P1.FIRST, sw2=Curve.PRIME256)
 
-class TestSignSSHBlob(Client):
+class TestSignSSHBlob:
     INS = Ins.SIGN_SSH_BLOB
 
     def test_sign_ssh_blob(self, client):
@@ -133,6 +112,5 @@ class TestSignSSHBlob(Client):
             payload += length.to_bytes(4, "big")
             payload += request
 
-        status_word, data = self.apdu_exchange(client, payload)
-        assert status_word == 0x9000
+        data = client.apdu_exchange(self.INS, payload, sw1=P1.FIRST, sw2=Curve.PRIME256)
         assert data.startswith(b"\x30")
